9d4f6e6ec42826116ad9b22ae9f97fa19e3ad32c
zetaBUE <127629749+zetaBUE@users.noreply.github.com>
2025-05-06 02:20:12 +0300

    rest of commit?

---
+++ src/main/java/com/prismix/common/model/network/FileTransferUploadResponse.java
@@ -0,0 +1,34 @@
+package com.prismix.common.model.network;
+
+import java.io.Serializable;
+
+public class FileTransferUploadResponse implements NetworkMessage, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final boolean accepted;
+    private final String fileName;
+    private final String transferId;
+
+    public FileTransferUploadResponse(boolean accepted, String fileName, String transferId) {
+        this.accepted = accepted;
+        this.fileName = fileName;
+        this.transferId = transferId;
+    }
+
+    @Override
+    public MessageType getMessageType() {
+        return MessageType.FILE_TRANSFER_UPLOAD_RESPONSE;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getTransferId() {
+        return transferId;
+    }
+}
